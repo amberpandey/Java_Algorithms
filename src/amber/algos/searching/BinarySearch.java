07e95960327954326509a2b9d37694f679f7b074
Amber <amberpandey@outlook.com>
2017-04-29 17:20:15 -0400

    Search Algo
    Implementation of search algorithms(Binary and Linear)

--- src/amber/algos/searching/BinarySearch.java
+++ src/amber/algos/searching/BinarySearch.java
@@ -1,13 +1,56 @@
 /*
  * General Algorithms in Java
- *
+ * BinarySearch.java
  */
 package amber.algos.searching;
 
 /**
  * @author Amber
- *
+ * 
+ * Implementation of Binary Search
  */
 public class BinarySearch {
+	/**
+	 * <p>
+	 * Binary Search 
+	 * Also Known as also known as half-interval search,logarithmic search or binary chop.
+	 * Binary search looks for a particular item by comparing the middle most item of the collection. 
+	 * If a match occurs, then the index of item is returned. If the middle item is greater than the item, 
+	 * then the item is searched in the sub-array to the left of the middle item. Otherwise, 
+	 * the item is searched for in the sub-array to the right of the middle item. 
+	 * This process continues on the sub-array as well until the size of the subarray reduces to zero.
+	 * </p>
+	 * <p>
+	 * Points :- 
+	 * 1. binary search is used to quickly find a value in a sorted sequence.
+	 * 2. Half of the list is removed before each comparison.
+	 * </p>
+	 * <p>
+	 * Complexities :-
+	 * Best Case Time complexity :- O(1)
+	 * Worst Case Time complexity :- O(log n)
+	 * Average Case Time Complexity :- O(log n)
+	 * Space Complexity :- O(1)
+	 * </p>
+	 * @param arrayOfNumbers Target list of numbers where we want to search particular number
+	 * @param noToSearch number we are looking to search in given list.
+	 * @return
+	 */
+	public static int binarySearch(int[] arrayOfNumbers, int noToSearch) {
+		int low = 0;
+		int high = arrayOfNumbers.length - 1;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (arrayOfNumbers[mid] == noToSearch) {
+				return mid;
+			} else if (arrayOfNumbers[mid] < noToSearch) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return -1;
+		
+	}
 
 }
--- src/amber/algos/searching/LinearSearch.java
+++ src/amber/algos/searching/LinearSearch.java
@@ -6,8 +6,62 @@ package amber.algos.searching;
 
 /**
  * @author Amber
- *
+ *         <p>
+ *         Unsorted Linear Search and Sorted/Ordered Linear Search. 
  */
 public class LinearSearch {
+	/**
+	 * Linear Search on unsorted Array of numbers
+	 * <p>
+	 * Linear search is a very simple search algorithm. 
+	 * In this type of search, a sequential search is made over all items one by one. 
+	 * Every item is checked and if a match is found then that particular item is returned, 
+	 * otherwise the search continues till the end of the data collection.
+	 * </p>
+	 * <p>
+	 * Complexities:-
+	 *     1. Time Complexity of linear search in worst case will be O(n). 
+     *     2. Space Complexity :- O(1)
+     * </p>
+	 * @param arrayOfNumbers Target list of numbers where we want to search particular number
+	 * @param noToSearch number we are looking to search in given list.
+	 * @return
+	 */
+	public static int unsortedLinearSearch(int arrayOfNumbers[], int noToSearch) {
+		for (int i = 0; i <= arrayOfNumbers.length; i++) {
+			if (noToSearch == arrayOfNumbers[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	/**
+	 * Linear Search on sorted Array of numbers
+	 * search fails as soon as any number becomes greater than the number to besearch in array.
+	 * <p>
+	 * Linear search is a very simple search algorithm. 
+	 * In this type of search, a sequential search is made over all items one by one. 
+	 * Every item is checked and if a match is found then that particular item is returned, 
+	 * otherwise the search continues till the end of the data collection.
+	 * </p>
+	 * <p>
+	 * Complexities:-
+	 *     1. Time Complexity of linear search in worst case will be O(n). 
+     *     2. Space Complexity :- O(1)
+     * </p>
+	 * @param arrayOfNumbers Target list of numbers where we want to search particular number
+	 * @param noToSearch number we are looking to search in given list.
+	 * @return -1 if search failure else index of number in array.
+	 */
+	public static int sortedLinearSearch(int arrayOfNumbers[], int noToSearch) {
+		for (int i = 0; i <= arrayOfNumbers.length; i++) {
+			if (noToSearch == arrayOfNumbers[i]) {
+				return i;
+			} else if (arrayOfNumbers[i] > noToSearch) {
+				return -1;
+			}
+		}
+		return -1;
+	}
 }
