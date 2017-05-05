4ca112a55c5e510e9debab262939ab93817159a6
Amber <amberpandey@outlook.com>
2017-05-05 14:06:51 -0400

    Bucket Sort

---
+++ src/amber/algos/sorting/BucketSort.java
@@ -0,0 +1,21 @@
+/*
+ * General Algorithms in Java
+ *
+ */
+package amber.algos.sorting;
+
+/**
+ * @author Amber
+ *
+ */
+public class BucketSort {
+	/**
+	 * 
+	 * @param arrayToSort
+	 * @return
+	 */
+	public static int[] doBucketSort(int[] arrayToSort){
+		return arrayToSort;
+	}
+
+}
